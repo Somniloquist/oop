cbed79419c1a847e2ca7a5bbbc9d37272c0b0068
Robin Unger <robin.e.unger@gmail.com>
2019-03-07 20:53:35 -0600

    Set players and turn order

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,4 +1,37 @@
 #!/usr/bin/env ruby
+class Board
+end
+
+class Player
+    attr_accessor :name, :win_count, :lose_count, :last_roll
+
+    def initialize(name)
+        self.name = name
+        self.win_count = 0
+        self.lose_count = 0
+        self.last_roll = 0
+    end
+
+    def roll_dice
+        self.last_roll = rand(1..100)
+    end
+end
+
+# returns an array that reflects the player turn order based on a dice roll
+def get_play_order(player1, player2)
+    players = [player1, player2]
+    players.sort! { |one, two| one.roll_dice <=> two.roll_dice }.reverse!
+end
 
 def game
+    # initialize the two players
+    player1 = Player.new("Player1")
+    player2 = Player.new("Player2")
+
+    # determine who goes first
+    players = get_play_order(player1, player2)
+    puts("#{player1.name} rolled #{player1.last_roll} ... #{player2.name} rolled #{player2.last_roll}.")
+    puts("#{players[0].name} goes first!")
 end
+
+game
