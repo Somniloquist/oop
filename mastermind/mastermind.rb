549de4966fc4216de84592fa8ae71ecc899bf508
Robin Unger <robin.e.unger@gmail.com>
2019-03-13 19:05:02 -0500

    Add main play method skeleton

--- mastermind/mastermind.rb
+++ mastermind/mastermind.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 module Mastermind
-  attr_accessor :value
   class Cell
+    attr_accessor :value
     def initialize(value = "")
       @value = value
     end
@@ -48,27 +48,45 @@ module Mastermind
   end
 
   class Game
+    attr_accessor :turn
     attr_reader :board, :player, :ai_player
     def initialize()
       @player = get_human_player
       @ai_player = get_ai_player
       @board = Board.new(secret: get_secret_code)
+      @turn = 1
     end
 
     def play
+      code_breaker, code_master = 1, 2
+      if player.role == code_breaker
+        guess = solicit_code { puts("Guess the 4 digit code.") }
+        if codes_match?(guess, board.secret)
+          puts("You win!")
+        else
+          puts("You lose.")
+        end
+      else
+        puts "Code Master Not yet implemented."
+        puts "Ending game."
+      end
     end
 
     private
     def get_human_player
       player_name = solicit_name
-      # assume player will always choose code breaker for now
-      player_role = 2 #solicit_role
+      player_role = solicit_role
       Player.new(name: player_name, role: player_role)
     end
 
     def get_ai_player
       self.player.role == 1 ? ai_role = 2 : ai_role = 1
       Player.new(name: "Computer", role: ai_role)
+    end
+
+    def codes_match?(guess, secret)
+      secret_code = secret.map { |cell| cell.value }
+      guess == secret_code
     end
 
     def solicit_role
@@ -84,11 +102,11 @@ module Mastermind
       gets.chomp
     end
 
-    def solicit_secret_code
-      puts("You are the code master, choose a secret code.")
+    def solicit_code
+      yield
       loop do
         print("Enter a 4 digit code: ")
-        # convert to_i to strop non number characters before converting to_a
+        # convert to_i to strip non number characters before converting to_a
         code = gets.chomp.to_i.to_s.split('')
         return code if code.length == 4
       end
@@ -96,7 +114,7 @@ module Mastermind
 
     def get_secret_code
       return get_random_code if self.player.role == 1
-      solicit_secret_code
+      solicit_code { puts "You are the Code Master. Choose a secret code."}
     end
 
     def get_random_code
@@ -110,13 +128,15 @@ module Mastermind
 end
 
 include Mastermind
+
 game = Game.new
+game.play
 
 # puts "====== TURN 1 ======="
-# board.decoding_grid << [Cell.new("1"), Cell.new("2"), Cell.new("3"), Cell.new("4")]
-# board.key_grid << [Cell.new, Cell.new, Cell.new, Cell.new]
-# board.print_formatted_board
+# game.board.decoding_grid << [Cell.new("1"), Cell.new("2"), Cell.new("3"), Cell.new("4")]
+# game.board.key_grid << [Cell.new, Cell.new, Cell.new, Cell.new]
+# game.board.print_formatted_board
 # puts "====== TURN 2 ======="
-# board.decoding_grid << [Cell.new("1"), Cell.new("2"), Cell.new("3"), Cell.new("4")]
-# board.key_grid << [Cell.new, Cell.new, Cell.new, Cell.new]
-# board.print_formatted_board
+# game.board.decoding_grid << [Cell.new("1"), Cell.new("2"), Cell.new("3"), Cell.new("4")]
+# game.board.key_grid << [Cell.new, Cell.new, Cell.new, Cell.new]
+# game.board.print_formatted_board
